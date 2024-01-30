0467ebc9f8a2d09b6b3529748d3fede20d8a8f77
Heng Yang <hankyang94@gmail.com>
2024-01-30 18:15:11 -0500

    add sedumi

---
+++ sedumi_example.m
@@ -0,0 +1,21 @@
+clc; clear; close all;
+
+addpath(genpath("../cvx/sedumi"))
+
+% describe dimensions of the cones
+K.f = 1; % free cone
+K.l = 2; % nonnegative orthant
+K.q = 4; % second order cone
+K.s = 3; % psd cone
+
+% provide A,b,c
+c = [3,-1,-1,0,-1,0,-1,1,0,0,0,2,0,0,0,3];
+b = [-1,0,1];
+A = [
+    1,-1,0,-1,-1,0,0,0,-1,0,-1,0,0,0,0,0;
+    1,-1,-1,0,0,-1,0,0,0,-1,0,0,0,-1,0,0;
+    1,0,-1,-1,0,0,-1,0,0,0,0,0,-1,0,-1,0
+    ];
+
+% solve using sedumi
+[xopt,yopt,info] = sedumi(A,b,c,K);
