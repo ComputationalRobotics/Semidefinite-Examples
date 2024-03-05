3f0e7f2e21d492491d59f7c5b26e2c18f19a70b5
Heng Yang <hankyang94@gmail.com>
2024-03-05 18:17:35 -0500

    add pop example

--- example_pop.m
+++ example_pop.m
@@ -5,23 +5,27 @@ addpath(genpath('../SOSTOOLS'))
 addpath(genpath('../../../mosek'))
 
 x = mpvar('x',2,1); % dummy x 
-h = [x'*x - 1];
-g = [3*x(2)-x(1)^3-2;
-     x(1)-8*x(2)^3];
-g = [g; g(1)*g(2)];
 
+h = [x(1)^2-1;
+     x(2)^2-1];
+g = [monomials(x,0);
+     x(1)];
+p = 4*x(1)^3 + 6*x(2);
 prog = sosprogram(x);
 
-kappa = 4;
+gam = dpvar('gam');
+prog = sosdecvar(prog,gam);
 
-lhs = 0;
+kappa = 2;
+
+lhs = p - gam;
 lams = {};
 for i = 1:length(h)
     hi = h(i);
     deg_hi = hi.maxdeg;
     deg_lam = 2*kappa - deg_hi;
     [prog,lam] = sospolyvar(prog,monomials(x,0:deg_lam));
-    lhs = lhs + lam*hi;
+    lhs = lhs - lam*hi;
     lams{end+1} = lam;
 end
 sigs = {};
@@ -31,20 +35,16 @@ for i = 1:length(g)
     if deg_gi <= 2*kappa
         deg_sig = floor((2*kappa - deg_gi)/2);
         [prog,sig] = sossosvar(prog,monomials(x,0:deg_sig));
-        lhs = lhs + sig*gi;
+        lhs = lhs - sig*gi;
         sigs{end+1} = sig;
     end
 end
 
-prog = soseq(prog,lhs+1);
+prog = soseq(prog,lhs);
+prog = sossetobj(prog,-gam);
 options.solver = 'mosek';
 prog = sossolve(prog,options);
+gam_val = sosgetsol(prog,gam);
 
-lams_val = {};
-for i = 1:length(lams)
-    lams_val{end+1} = cleanpoly(sosgetsol(prog,lams{i}),1e-6);
-end
-sigs_val = {};
-for i = 1:length(sigs)
-    sigs_val{end+1} = cleanpoly(sosgetsol(prog,sigs{i}),1e-6);
-end
+
+
